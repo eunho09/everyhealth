45b25442ba188f8856873a5d554818983619fa34
eunho09 <Latte09@gmail.com>
2025-05-14 23:41:16 +0900

    update REACT_APP env

--- src/main/frontend/public/config.js
+++ src/main/frontend/public/config.js
@@ -1,4 +1,4 @@
 window.env={
-    REACT_API_BASEURL:"https://alb.everyhealth.net",
-    REACT_GOOGLE_LOGIN_URL:"https://alb.everyhealth.net/oauth2/authorization/google",
+    REACT_APP_API_BASEURL:"https://alb.everyhealth.net",
+    REACT_APP_GOOGLE_LOGIN_URL:"https://alb.everyhealth.net/oauth2/authorization/google",
 }
